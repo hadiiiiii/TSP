a0123c58fbbacd194f07bbb79e1a3ab15aa844ec
hadiiiiii <45215673+hadiiiiii@users.noreply.github.com>
2018-11-23 17:59:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,17 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
+const CodingXActivity = [
+  "minecraft", //IT's Broken Here
+  "PUBG",
+  ];
+  
+client.on('ready', () => {
+  setInterval(() => {
+      const index = Math.floor(Math.random() * (CodingXActivity.length - 1) + 1);
+ client.user.setGame(CodingXActivity[index], "https://www.twitch.tv/CodingXArea");
+  }, 10000); //10 seconds
+});
 
 
 
